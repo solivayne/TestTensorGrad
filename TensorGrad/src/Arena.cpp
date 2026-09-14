a063ab44ee2ed39b6386baa156d452a9749d90ca
solivayne <albertfriedrich656@gmail.com>
2026-09-14 16:47:01 +0800

    fix: fix arena bugs for allocate gpu memory

--- TensorGrad/src/Arena.cpp
+++ TensorGrad/src/Arena.cpp
@@ -7,53 +7,64 @@ Arena globalArena(100000000);
 Arena paramArena(100000000);
 
 Arena::Arena(size_t maxElements)
-    : capacity(maxElements), 
+    : capacity(maxElements),
       cpuOffset(0),
       cpuMemory(std::make_unique<float[]>(maxElements)),
       gpuOffset(0),
       gpuMemory(nullptr) {}
 
-Arena::~Arena() {
-    if (gpuMemory != nullptr) {
+Arena::~Arena()
+{
+    if (gpuMemory != nullptr)
+    {
         freeVram(gpuMemory);
     }
 }
 
-float *Arena::allocate(size_t numElements, Device device) {
-    if (device == Device::CPU) {
+float *Arena::allocate(size_t numElements, Device device)
+{
+    if (device == Device::CPU)
+    {
         if (cpuOffset + numElements > capacity)
             throw std::runtime_error(
                 "Arena out of memory, please increase capacity.");
         float *ptr = cpuMemory.get() + cpuOffset;
         cpuOffset += numElements;
         return ptr;
-    } else {
+    }
+    else
+    {
         if (gpuOffset + numElements > capacity)
             throw std::runtime_error("Arena GPU out of memory, please increase capacity.");
-        
-        if (gpuMemory == nullptr) {
+
+        if (gpuMemory == nullptr)
+        {
             std::cout << "Arena initializing " << (capacity * 8.0 / 1024 / 1024) << "MB out of VRAM" << std::endl;
             gpuMemory = allocateVram(capacity * sizeof(float));
-            if (gpuMemory == nullptr) {
+            if (gpuMemory == nullptr)
+            {
                 throw std::runtime_error("Fatal: cudaMalloc failed to allocate VRAM.");
             }
         }
 
-        float *ptr = gpuMemory;
+        float *ptr = gpuMemory + gpuOffset;
         gpuOffset += numElements;
         return ptr;
     }
 }
 
-void Arena::reset() {
-    cpuOffset = 0; 
+void Arena::reset()
+{
+    cpuOffset = 0;
     gpuOffset = 0;
 }
 
-void Arena::print_usage() const {
+void Arena::print_usage() const
+{
     std::cout << "Arena CPU usage: " << cpuOffset << " / " << capacity << " ("
               << (cpuOffset * 8.0 / 1024 / 1024) << " MB)\n";
-    if (gpuMemory != nullptr) {
+    if (gpuMemory != nullptr)
+    {
         std::cout << "Arena GPU usage: " << gpuOffset << " / " << capacity << " ("
                   << (gpuOffset * 8.0 / 1024 / 1024) << " MB)\n";
     }
